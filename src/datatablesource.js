da6fc59a5616d21570d1ad9c7a48602a00522a6f
Imanda03 <asis03ktm@gmail.com>
2023-06-17 22:30:51 +0545

    admin pannel

---
+++ src/datatablesource.js
@@ -0,0 +1,94 @@
+export const userColumns = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img
+            className="cellImg"
+            src={
+              params.row.img ||
+              "https://static.vecteezy.com/system/resources/previews/009/330/731/original/avatar-icon-profile-icon-member-login-isolated-login-icons-profile-icons-free-vector.jpg"
+            }
+            alt="avatar"
+          />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 230,
+  },
+
+  {
+    field: "country",
+    headerName: "Country",
+    width: 100,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 100,
+  },
+];
+
+export const hotelColumns = [
+  { field: "_id", headerName: "ID", width: 250 },
+  {
+    field: "name",
+    headerName: "Name",
+    width: 150,
+  },
+
+  {
+    field: "type",
+    headerName: "Type",
+    width: 150,
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 150,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 150,
+  },
+];
+
+export const roomColumns = [
+  { field: "_id", headerName: "ID", width: 70 },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 230,
+  },
+
+  {
+    field: "desc",
+    headerName: "Description",
+    width: 100,
+  },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 100,
+  },
+  {
+    field: "maxPeople",
+    headerName: "Max People",
+    width: 100,
+  },
+];
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import "./home.scss";
+import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <h2 className="sideHomePage">Home Page</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
